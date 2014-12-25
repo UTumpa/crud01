7ce070fd15573a9aeabd07749a2d8fd8061efae3
Shajed <ctgboy2010@gmail.com>
2014-12-25 09:14:55 +0600

    initial work

---
+++ form.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<table border="1", style="width:60%">
+    <tr>
+        <td>Jill</td>
+        <td>Smith</td>
+
+    </tr>
+    <tr>
+        <td>Eve</td>
+        <td>Jackson</td>
+
+    </tr>
+<tr>
+    <td>Jill</td>
+    <td>Smith</td>
+</tr>
+<tr>
+    <td>Jill</td>
+    <td>Smith</td>
+</tr>
+<tr>
+    <td>Jill</td>
+    <td>Smith</td>
+    </tr>
+</table>
+
+</body>
+</html>
